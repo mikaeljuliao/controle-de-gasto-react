569f7b134f4424c4b09c998826089f6ba0133aab
mikael <mikaeljuliao56@gmail.com>
2025-09-23 00:20:54 -0300

    lista de gasto agora está funcionando como uma coluna

--- src/componentes/ListaGastos.jsx
+++ src/componentes/ListaGastos.jsx
@@ -4,45 +4,47 @@ export default function ListaGastos({ gastos, aoRemover, aoEditar }) {
   return (
     <ul className="list-group">
       {gastos.map((gasto) => (
-        <li
-          key={gasto.id}
-          className="list-group-item d-flex justify-content-between align-items-center"
-        >
-          {/* Descrição */}
-          <span className='me-3'>{gasto.descricao}</span>
+       <li key={gasto.id} className="list-group-item">
+  <div className="row align-items-center">
+    
+    {/* Coluna Descrição */}
+   <div className="col-4 text-break">
+  {gasto.descricao}
+   </div>
 
-          {/* Valor + Tipo */}
-          <span
-            className={
-              gasto.tipo === "Receita"
-                ? "text-success fw-bold"
-                : "text-danger fw-bold"
-            }
-          >
-            {gasto.valor.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            })}{" "}
-            ({gasto.tipo})
-          </span>
+    {/* Coluna Valor */}
+    <div className="col-3">
+      <span className={gasto.tipo === "Receita" ? "text-success fw-bold" : "text-danger fw-bold"}>
+        {gasto.valor.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </span>
+    </div>
 
-          {/* Botões */}
-          <div>
-            <button
-              className="btn btn-sm btn-outline-primary me-2"
-              onClick={() => aoEditar(gasto.id)}
-            >
-              ✏️
-            </button>
+    {/* Coluna Tipo */}
+    <div className="col-3">
+      <span>{gasto.tipo}</span>
+    </div>
 
-            <button
-              className="btn btn-sm btn-danger"
-              onClick={() => aoRemover(gasto.id)}
-            >
-              ❌
-            </button>
-          </div>
-        </li>
+    {/* Coluna Ações */}
+    <div className="col-2 d-flex justify-content-end gap-2">
+      <button
+        className="btn btn-sm btn-outline-primary"
+        onClick={() => aoEditar(gasto.id)}
+      >
+        ✏️
+      </button>
+      <button
+        className="btn btn-sm btn-danger"
+        onClick={() => aoRemover(gasto.id)}
+      >
+        ❌
+      </button>
+    </div>
+  </div>
+</li>
+
       ))}
     </ul>
   )
